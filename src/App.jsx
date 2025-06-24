1065a03b49640910b1602103b16761a9f2d48610
Fakhreza.Susilo <fakhreza.susilo@cermati.com>
2025-06-24 10:51:45 +0700

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
           <div className="mb-6">
             <h3 className="text-xl font-semibold">Bug Report Samples</h3>
-            <img src="assets/Screenshot 2025-06-16 at 21.11.31.png" alt="Tokopedia Bug Report" className="rounded shadow mt-2" />
+            <img src="assets/Screenshot_2025-06-16_at_21.10.01.png" alt="Tokopedia Bug Report" className="rounded shadow mt-2" />
           </div>
         </section>
 
