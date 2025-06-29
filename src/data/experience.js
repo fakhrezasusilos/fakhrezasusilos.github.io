5be191b1d6f297c2855482b8a88a03694e15d2f0
Fakhreza.Susilo <fakhreza.susilo@cermati.com>
2025-06-29 15:53:43 +0700

    add experience

---
+++ src/data/experience.js
@@ -0,0 +1,58 @@
+const experience = [
+  {
+    id: 1,
+    icon: "/assets/cermati_logo.png",
+    role: "Quality Assurance Engineer",
+    company: "PT. Dwi Cermat Indonesia ( Cermati.com )",
+    location: "Jakarta, Indonesia - Remote",
+    startDate: "Feb 2020",
+    endDate: "Present",
+    description: "Responsible for Automation Testing on Cermati Web and Mobile Apps.Sometimes handle Manual Testing include create test cases, exploratory testing, API Testing and regression testing.",
+    techStack: "Appium , WebdriverIO , Cucumber , Postman , Altair GraphQL, Phabricator, Google Spreadsheet"
+  },
+  {
+    id: 2,
+    icon: "/assets/mamikos_logo.png",
+    role: "Senior Quality Assurance",
+    company: "Mamikos",
+    location: "Yogyakarta, Indonesia - On Site",
+    startDate: "July 2019",
+    endDate: "Feb 2020",
+    description: "Perform Manual Testing on Mamikos Web and Mobile Apps.Backup Lead for team management ( Distribute tasks to team members, monitor progress, and ensure team members are focused on their assigned tasks ).",
+    techStack: "Google Spreadsheet , Jira , Postman"
+  },
+  {
+    id: 3,
+    icon: "/assets/cermati_logo.png",
+    role: "Quality Assurance",
+    company: "PT. Dwi Cermat Indonesia ( Cermati.com )",
+    location: "Jakarta, Indonesia - On Site",
+    startDate: "November 2018",
+    endDate: "July 2019",
+    description: "Perform Manual Testing on Cermati Web and Mobile Apps.",
+    techStack: "Google Spreadsheet , TestRail , Postman"
+  },
+  {
+    id: 4,
+    icon: "/assets/gameloft_logo.png",
+    role: "QA Engineer",
+    company: "Gameloft Indonesia",
+    location: "Yogyakarta, Indonesia - On Site",
+    startDate: "October 2018",
+    endDate: "November 2017",
+    description: "Perform Automation Testing on Mobile Games android based on test case",
+    techStack: "Google Spreadsheet,Office provided tools"
+  },{
+    id: 5,
+    icon: "/assets/gameloft_logo.png",
+    role: "Quality Assurance",
+    company: "Gameloft Indonesia",
+    location: "Yogyakarta, Indonesia - On Site",
+    startDate: "October 2011",
+    endDate: "October 2017",
+    description: "Create test case , exploratory test and testcase based testing. Search bug on Mobile games then report it to bug base",
+    techStack: "Google Spreadsheet"
+  }
+];
+
+export default experience;
