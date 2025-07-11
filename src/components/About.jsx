b4b2698d0c8229dc5760d0d2b94beee63fa88f28
Fakhreza.Susilo <fakhreza.susilo@cermati.com>
2025-07-11 15:30:22 +0700

    update skill and about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,7 @@ const About = () => {
     <section id="about" className="bg-white rounded-2xl shadow p-6">
       <h2 className="text-2xl font-bold text-[#2c3e50] mb-4 border-b border-gray-200 pb-2">About Me</h2>
       <p>
-        I am a passionate Quality Assurance Engineer from Indonesia with experience in both manual and automation testing.
-        I love ensuring software quality through clear communication, structured test planning, and clean automation code.
+         6+ years experience as a QA Engineer, currently working at a fintech company (Cermati.com). Skilled in manual testing (Web, Mobile, API) and automation testing using WebdriverIO (with Cucumber), Appium, and Playwright. Proficient with test design, bug reporting, and project collaboration
       </p>
     </section>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ const Skills = () => {
       <h2 className="text-2xl font-bold text-[#2c3e50] mb-4 border-b border-gray-200 pb-2">Skills & Tools</h2>
       <ul className="list-disc ml-6 space-y-1">
         <li><strong>Manual Testing:</strong> Test case writing, exploratory testing, regression testing</li>
-        <li><strong>Automation:</strong> WebdriverIO, Cucumber, Appium</li>
+        <li><strong>Automation:</strong> WebdriverIO, Cucumber, Appium , Playwright</li>
         <li><strong>API Testing:</strong> Postman, REST Assured</li>
         <li><strong>Tools:</strong> Jira, TestRail, Git, GitHub Actions</li>
       </ul>
