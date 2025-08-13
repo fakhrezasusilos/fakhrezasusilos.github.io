b160464a07aad840a1b12b108d167da512fc7b63
Fakhreza.Susilo <fakhreza.susilo@cermati.com>
2025-08-13 13:54:16 +0700

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ const Resume = () => {
   return (
     <section id="resume" className="bg-white rounded-2xl shadow p-6">
       <h2 className="text-2xl font-bold text-[#2c3e50] mb-4 border-b border-gray-200 pb-2">Resume</h2>
-      <p><a href="https://drive.google.com/file/d/1pTxn5tI_kfv-Z8hCPurRNRnZd3DBGOaN/view?usp=sharing" className="text-blue-600 underline">Download My CV (PDF)</a></p>
+      <p><a href="https://drive.google.com/file/d/1ADLNQpEX2WBbz9WRj16U4z-86VCUcKBq/view?usp=sharing" className="text-blue-600 underline">Download My CV (PDF)</a></p>
     </section>
   );
 }
